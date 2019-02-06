15f3704c76b0dc0868662e4c9817746a45326c31
michael.sauer@htwsaar.de <michael.sauer@htwsaar.de>
2019-02-06 17:02:19 +0100

    Initiales Java-Menue-Programm für Programmierteam WiSe2018/19

---
+++ menue.java
@@ -0,0 +1,36 @@
+/*
+ * Gerüst für ein kleines Java-Menue um mit Git VCS zu lernen
+ * Michael Sauer, 22.01.2016
+ * 
+ */
+import java.util.Scanner;
+/**
+ *
+ * @author mike
+ */
+
+
+public class menue{
+        public static void main(String[] args) {
+        	final int PROGRAMMENDE=0;
+        	boolean isEnde=false;
+        	while(!isEnde){
+                	Scanner scanner = new Scanner(System.in);
+                	int wahl = scanner.nextInt();
+                	switch (wahl) {
+                        	case PROGRAMMENDE:
+                                	System.out.println("Ciao");
+                                	isEnde=true;
+                        	break;
+                        	case 1:
+                                	System.out.println("Michael Sauer");
+                        	break;
+                        	case 2:
+              //wer da sonst noch so wäre ...
+                        	break;
+                        	default:
+                                	System.out.println("Fehler: Kenne ich nicht!");
+                        	}
+                	}
+        }
+	}
