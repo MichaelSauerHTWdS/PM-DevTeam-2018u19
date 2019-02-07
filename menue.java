eb82ac6c025dedbc7f08701fcef141a33689e37c
Matthias Riegler <me@xvzf.tech>
2019-02-07 14:56:42 +0100

    Rollback

--- menue.java
+++ menue.java
@@ -1,55 +1,38 @@
 /*
  * Gerüst für ein kleines Java-Menue um mit Git VCS zu lernen
  * Michael Sauer, 22.01.2016
- *
+ * 
  */
-
 import java.util.Scanner;
-
 /**
+ *
  * @author mike
  */
 
 
-public class menue {
-    public static void main(String[] args) {
-        final int PROGRAMMENDE = 0;
-        boolean isEnde = false;
-        Scanner scanner = new Scanner(System.in);
-        while (!isEnde) {
-            int wahl = scanner.nextInt();
-            switch (wahl) {
-                case PROGRAMMENDE:
-                    System.out.println("Ciao");
-                    isEnde = true;
-                    break;
-                case 1:
-                    System.out.println("Michael Sauer");
-                    break;
-                case 2:
-                    System.out.println("Roland Daidone");
-                    //wer da sonst noch so wäre ..
-                    break;
-                case 14:
-                    System.out.println("Shenna R W P");
-                    break;
-                case 18:
-                    System.out.println("Matthias Sennikow\n aka en voll krasser motherfucker");
-                    break;
-                case 20:
-                    System.out.println("Tobias Kiel");
-                    break;
-                case 22:
-                    System.out.println("Anne-Kathrin Haag");
-                    break;
-
-                case 28:
-                    System.out.println("Julian Krieger");
-                default:
-                    System.out.println("Fehler: Kenne ich nicht!");
-            }
+public class menue{
+        public static void main(String[] args) {
+        	final int PROGRAMMENDE=0;
+			boolean isEnde=false;
+			Scanner scanner = new Scanner(System.in);
+        	while(!isEnde){
+                	int wahl = scanner.nextInt();
+                	switch (wahl) {
+                        	case PROGRAMMENDE:
+                                	System.out.println("Ciao");
+                                	isEnde=true;
+                        	break;
+                        	case 1:
+                                	System.out.println("Michael Sauer");
+                        	break;
+                        	case 2:
+              //wer da sonst noch so wäre ...
+                        	break;
+                        	default:
+                                	System.out.println("Fehler: Kenne ich nicht!");
+                        	}
+					}
+				scanner.close();
+			
         }
-        scanner.close();
-
-    }
-}
+	}
