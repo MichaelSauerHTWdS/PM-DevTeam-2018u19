b00a919289cd72639cd5ed228bca8c27ff698fa2
Test <test@test.com>
2019-02-07 14:11:55 +0100

    LOL

--- menue.java
+++ menue.java
@@ -12,30 +12,31 @@ import java.util.Scanner;
 
 public class menue{
         public static void main(String[] args) {
-        	final int PROGRAMMENDE=0;
-			boolean isEnde=false;
-			Scanner scanner = new Scanner(System.in);
-        	while(!isEnde){
-                	int wahl = scanner.nextInt();
-                	switch (wahl) {
-                        	case PROGRAMMENDE:
-                                	System.out.println("Ciao");
-                                	isEnde=true;
-                        	break;
-                        	case 1:
-                                	System.out.println("Michael Sauer");
-                        	break;
-                        	case 2:
+            final int PROGRAMMENDE=0;
+            boolean isEnde=false;
+            Scanner scanner = new Scanner(System.in);
+            while(!isEnde){
+                    int wahl = scanner.nextInt();
+                    switch (wahl) {
+                            case PROGRAMMENDE:
+                                    System.out.println("Ciao");
+                                    isEnde=true;
+                            break;
+                            case 1:
+                                    System.out.println("Michael Sauer");
+                            break;
+                            case 2:
+                                    System.out.println("Roland Daidone");                           
               //wer da sonst noch so wäre ...
-                        	break;
-							case 22:
-									System.out.println("Anne-Kathrin Haag");
-									break;
-                        	default:
-                                	System.out.println("Fehler: Kenne ich nicht!");
-                        	}
-					}
-				scanner.close();
-			
+                            break;
+                            case 22:
+                                    System.out.println("Anne-Kathrin Haag");
+                                    break;
+                            default:
+                                    System.out.println("Fehler: Kenne ich nicht!");
+                            }
+                    }
+                scanner.close();
+            
         }
-	}
+    }
