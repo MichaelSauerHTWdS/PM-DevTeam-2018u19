cecfa6306ea2d7dbfabccb15af86fb92ac1d6619
rdaidone <rdaidone@kil-m-12.htw-saarland.de>
2019-02-07 15:18:21 +0100

    Mein Name ist eingefuegt.

--- menue.java
+++ menue.java
@@ -25,6 +25,9 @@ public class menue{
                         	case 1:
                                 	System.out.println("Michael Sauer");
                         	break;
+				            case 22:
+                                        System.out.println("Roland Daidone");
+                           break;
                         	case 2:
               //wer da sonst noch so wäre ...
                         	break;
