da2d8bb269d10e7b6a544bba6c4686342e2180dd
Zoralink99 <alessioragusa@web.de>
2019-02-07 15:18:16 +0100

    Alessio Ragusa

--- menue.java
+++ menue.java
@@ -25,6 +25,9 @@ public class menue{
                         	case 1:
                                 	System.out.println("Michael Sauer");
                         	break;
+                        	case 38:
+                                	System.out.println("Alessio Ragusa");
+                        	break;
                         	case 2:
               //wer da sonst noch so wäre ...
                         	break;
