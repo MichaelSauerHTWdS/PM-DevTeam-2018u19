3bd8356bc4e97048dbe12b1bcaf9476b25939397
juliankrieger <jkrieger@htwsaar.de>
2019-02-07 15:07:16 +0100

    add Julian Krieger

--- menue.java
+++ menue.java
@@ -28,6 +28,9 @@ public class menue{
                         	case 2:
               //wer da sonst noch so wäre ...
                         	break;
+				case 28:
+					System.out.println("Julian Krieger");
+					break;
                         	default:
                                 	System.out.println("Fehler: Kenne ich nicht!");
                         	}
