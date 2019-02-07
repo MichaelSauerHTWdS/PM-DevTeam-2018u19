5ffcdc12cf4bfa275bbc85b3ec3ea9304c9372ca
Patrick Plewka <patrickplewka@googlemail.com>
2019-02-07 15:15:45 +0100

    Added my name

--- menue.java
+++ menue.java
@@ -25,6 +25,9 @@ public class menue{
                         	case 1:
                                 	System.out.println("Michael Sauer");
                         	break;
+                        	case 12:
+                                	System.out.println("Patrick Plewka");
+                        	break;
                         	case 2:
               //wer da sonst noch so wäre ...
                         	break;
