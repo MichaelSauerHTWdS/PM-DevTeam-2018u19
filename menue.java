572fbac844067b77c4311ec9d9f884fb511c5121
uU <Author@exsmple.com>
2019-02-07 15:23:24 +0100

    My case exists.

--- menue.java
+++ menue.java
@@ -25,6 +25,9 @@ public class menue{
                         	case 1:
                                 	System.out.println("Michael Sauer");
                         	break;
+                        	case 8:
+                            	System.out.println("Bleibt Annonym Uu.");
+                            	break;
                         	case 2:
               //wer da sonst noch so wäre ...
                         	break;
